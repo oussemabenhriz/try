69d30b087745cf78ea92d33b3706a548f8f5e7be
oussemabenhriz <oussemabenhariz33@gmail.com>
2022-12-19 18:42:44 +0100

    first react2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 //import logo from './logo.svg';
 import './App.css';
+import Aboutus from './component/Aboutus';
+import Contact from './component/Contact';
+import Footer from './component/Footer';
 import Navbar from "./component/Navbar";
 import Projet from "./component/Projet.js";
+
 
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
+     <Aboutus />
       <Projet />
+      <Contact />
+      <Footer />
+
     </div>
   );
 }
--- src/component/Projet.js
+++ src/component/Projet.js
@@ -1,19 +1,21 @@
 import React from "react";
-import Projet1 from "./component/Projet/Projet1.js";
-import Projet2 from "./component/projet/Projet2.js";
-import Projet3 from "./component/projet/Projet3";
-import Projet4 from "./component/projet/Projet4";
-import "../App.css";
+
+import Projet1 from "./projet/Projet1.js";
+import Projet2 from "./projet/Projet2.js";
+import Projet3 from "./projet/Projet3.js";
+import Projet4 from "./projet/Projet4.js";
 
 
-const Projet =() =>{
+const Projet = () =>{
     return(
-       <div>
+       <div id="projects">
+      <h2 class="text-important">Projects</h2>
+      <div class="projects-container">
         <Projet1 />
         <Projet2 />
         <Projet3 />
         <Projet4 />
-
+        </div>
        </div>
     )
 }
