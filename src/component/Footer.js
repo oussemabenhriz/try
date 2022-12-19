894801dd18c512cbbf236427c14fced722383683
oussemabenhriz <oussemabenhariz33@gmail.com>
2022-12-19 18:47:51 +0100

    challenge

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer =() =>{
     return(
         <footer>
-        <p>Copyritghts &copy; GOMYCODE 2020</p>
+        <p>Copyritghts &copy; GOMYCODE 2021</p>
       </footer>
     )
 }
